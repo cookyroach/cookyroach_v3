f83744ff2c7d1d3fdab2968ae26bcb25e9afa4d4
cookyroach <heycatchy@gmail.com>
2023-07-23 15:16:04 +0200

    comic style design

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,5 +4,32 @@
   :global(body) {
     margin: 0;
     font-family: Verdana, Geneva, Tahoma, sans-serif;
+    font-family: "Gaegu", cursive;
+    line-height: 1.1;
+    font-size: 1.5rem;
+  }
+
+  :global(h1) {
+    font-size: 3rem;
+    margin: 1rem 0;
+  }
+
+  :global(h2) {
+    font-size: 2.5rem;
+    margin: 0.5rem 0;
+  }
+
+  :global(h3) {
+    font-size: 2rem;
+    margin: 0.5rem 0;
+  }
+
+  :global(p) {
+    font-size: 1.5rem;
+  }
+
+  :global(a, button) {
+    font-size: inherit;
+    font-family: inherit;
   }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,105 @@
+<header>cookyroach</header>
 <main>
-  <div class="container">
-    <h1>Cookyroach</h1>
-    <p>Hi! I'm a comic creator & illustrator</p>
+  <div class="container panel">
+    <h1>Hi! I'm a comic creator & illustrator</h1>
+    <img src="img/roachy.png" width="175" alt="Roachy" />
+  </div>
+  <!-- <img
+    class="panel"
+    style="height: 250px; padding: 0;"
+    src="img/spaghetti.jpg"
+    alt=""
+  /> -->
+  <div class="panel">
+    <h2>Pet Rocky</h2>
+    <p>My cute comic, read it here:</p>
+    <button>Tapas</button>
+    <button>Webtoons</button>
+  </div>
+  <!-- <img src="img/pupu_nurse.JPG" alt="" /> -->
+  <div class="panel" style="background: #fff6e5;">
+    <h2>Support me</h2>
+    <img width="150" style="margin-bottom: 1rem" src="img/coin.png" alt="" />
+    <div>
+      <a
+        class="button"
+        href="https://www.redbubble.com/people/cookyroach/shop?asc=u"
+        >Redbubble shop</a
+      >
+      <a class="button" href="https://ko-fi.com/cookyroach">Ko-Fi </a>
+    </div>
   </div>
 </main>
+<footer>
+  <h3>My social media</h3>
+  <ul>
+    <li><a href="https://www.instagram.com/cookyroach/">Instagram</a></li>
+    <li><a href="https://twitter.com/thecookyroach">Twitter</a></li>
+    <li><a href="https://www.tiktok.com/@cookyroach">Tiktok</a></li>
+  </ul>
+  <blockquote>
+    bringing you the best comics <br /> we can pull out of our assets
+  </blockquote>
+</footer>
 
 <style>
+  header {
+    text-align: center;
+    padding: 1rem;
+    font-size: 2.25rem;
+    /* font-weight: bold; */
+  }
+
+  button,
+  .button {
+    background: #ffd97b;
+    border: 2.5px solid black;
+    border-radius: 24px 5px 31px 4px/4px 18px 4px 18px;
+    font-weight: bold;
+    font-size: 1.5rem;
+    padding: 0.25rem 0.5rem;
+    text-decoration: none;
+    color: inherit;
+  }
+
   .container {
     max-width: 800px;
     margin: auto;
+    padding: 1rem;
+    background: #ffeaef;
+  }
+
+  .panel {
+    border: 2.5px solid black;
+    border-radius: 24px 5px 31px 4px/4px 18px 4px 18px;
+    margin: 0 1rem 2rem;
+    padding: 1rem;
+    text-align: center;
+  }
+
+  img {
+    max-width: 100%;
+  }
+
+  footer {
+    text-align: center;
+    padding: 1rem 1rem 2rem;
+  }
+
+  footer ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  footer a {
+    color: #e26788;
+  }
+
+  blockquote {
+    font-style: italic;
+    font-size: 1.25rem;
+    margin: 0;
+    margin-top: 2rem;
   }
 </style>
